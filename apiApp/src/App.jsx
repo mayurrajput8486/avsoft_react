69fdd1d72418ea2714043d6da61d475843d31995
mayurrajput8486 <mayurrajput8486@gmail.com>
2026-02-28 17:42:41 +0530

    tailwindcss and start with the extr features like filter, sort and search

--- apiApp/src/App.jsx
+++ apiApp/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Products from './components/Products'
-
+/* import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js' */
 const App = () => {
   return (
     <div>
-      <h1>Learn API Fetching</h1>
+      <h1 className='text-center text-3xl font-bold text-orange-400'>Learn API Fetching</h1>
       <Products/>
     </div>
   )
--- apiApp/src/components/Products.jsx
+++ apiApp/src/components/Products.jsx
@@ -2,27 +2,40 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Products = () => {
-  const [details, setdetails] = useState([]);
+  const [products, setProducts] = useState([]);
   //loading
-  const [loading, setloading] = useState(false);
+  const [loading, setLoading] = useState(false);
   //error
-  const [error, seterror] = useState("");
+  const [error, setError] = useState("");
+  /* Features - search, filter, sort */
+  const [search, setSearch] = useState('')
+  //to store the category we use useState () hook
+  const [categories, setCategories] = useState([])
+  //to change the data accroding to the selected category
+  const [category, setCategory] = useState('')
+  //for sorting - high to low and low to high
+  const [sortprice, setsortPrice] = useState('')
 
   const baseUrl = "https://dummyjson.com/products";
 
   const getProductData = async () => {
-    setloading(true);
-    seterror("");
+    setLoading(true);
+    setError("");
     try {
       const response = await axios.get(baseUrl);
       //console.log(response)
       const result = response.data;
-      console.log(result.products);
-      setdetails(result.products);
+      //console.log(result.products);
+      setProducts(result.products);
+
+      //separate unique category 
+      const uniqueCategories = [...new Set(result.products.map((prod)=>prod.category))]
+      //console.log(uniqueCategories)
+      setCategories(uniqueCategories)
     } catch (error) {
-      seterror("Failed to get products details");
+      setError("Failed to get products details, check the url once !!!");
     } finally {
-      setloading(false);
+      setLoading(false);
     }
   };
 
@@ -30,42 +43,73 @@ const Products = () => {
     getProductData();
   }, []);
 
+  /* bootstrap version spinner - install bootstrap package, import bootstrap file  */
+  /* 
+    if(loading) {
+    return <div class="spinner-border text-primary" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+    }
+  */
+
+  if (loading) return <h3>Loading....</h3>;
+  if (error) return <h3 style={{ color: "red" }}>{error}</h3>;
   return (
     <div>
-      {loading && <h3>Loading.....</h3>}
-      {error && <h3 style={{ color: "red" }}>{error}</h3>}
-      <h2>Product Details</h2>
-      <table
-        border="2px"
-        style={{
-          width: "100%",
-          borderCollapse: "collapse",
-        }}
-      >
-        <thead>
+      <h2 className="text-center text-2xl font-semibold mt-3 text-blue-500">Product Details</h2>
+      <div className="mx-5 mb-5 flex gap-2 bg-black p-3 text-white rounded-2xl justify-center">
+
+        {/* For search the products */}
+        <input
+          type="text"
+          placeholder="Enter Item Title"
+          className="border-2 p-2 rounded-2xl border-white "
+        />
+
+        {/* Filter products on category */}
+        <select className="border-2 p-2 rounded-2xl w-50 text-blue-400">
+          <option value="">All</option>
+          {
+            categories.map((c, index)=>{
+              return(
+                <option key={index} value={c}>{c}</option>
+              )
+            })
+          }
+        </select>
+
+        {/* Sort data accriding to price */}
+        <select className="border-2 p-2 rounded-2xl w-50">
+          <option>Sorting</option>
+        </select>
+      </div>
+      <table className="border-2 table-auto mx-5 text-center">
+        <thead className="border-2 ">
           <tr>
-            <th>Product ID</th>
-            <th>Product Image</th>
-            <th>Product Titlepr</th>
-            <th>Product Description</th>
-            <th>Price</th>
-            <th>Rating</th>
-            <th>Warrenty</th>
+            <th className="border-2 px-2 py-2">Product ID</th>
+            <th className="border-2 px-2 py-2">Product Image</th>
+            <th className="border-2 px-2 py-2">Product Title</th>
+            <th className="border-2 px-2 py-2">Category</th>
+            <th className="border-2 px-2 py-2">Product Description</th>
+            <th className="border-2 px-2 py-2">Price</th>
+            <th className="border-2 px-2 py-2">Rating</th>
+            <th className="border-2 px-2 py-2">Warrenty</th>
           </tr>
         </thead>
         <tbody>
-          {details.map((product) => {
+          {products.map((product) => {
             return (
               <tr key={product.id}>
-                <td>{product.id}</td>
-                <td>
-                  <img src={product.thumbnail} height="150px" loading="lazy" />
+                <td className="border-2">{product.id}</td>
+                <td className="border-2">
+                  <img src={product.thumbnail} alt={product.title} height="150px" loading="lazy" />
                 </td>
-                <td>{product.title}</td>
-                <td>{product.description}</td>
-                <td>${product.price}</td>
-                <td>{product.rating}</td>
-                <td>{product.warrantyInformation}</td>
+                <td className="border-2">{product.title}</td>
+                <td className="border-2">{product.category}</td>
+                <td className="border-2">{product.description}</td>
+                <td className="border-2">${product.price}</td>
+                <td className="border-2">{product.rating}</td>
+                <td className="border-2">{product.warrantyInformation}</td>
               </tr>
             );
           })}
