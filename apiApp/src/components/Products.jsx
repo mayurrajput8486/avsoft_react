794c45f6967192e70a6832fd1a4c8ca32d41fa41
mayurrajput8486 <mayurrajput8486@gmail.com>
2026-03-02 19:44:07 +0530

    pagination logic

--- apiApp/src/components/Products.jsx
+++ apiApp/src/components/Products.jsx
@@ -10,23 +10,32 @@ const Products = () => {
 
 
   /* Features - search, filter, sort */
-  const [search, setSearch] = useState("");
-  //to change the data accroding to the selected category
-  const [selectedCategory, setSelctedCategory] = useState("");
-  //for sorting - high to low and low to high
-  const [sortprice, setsortPrice] = useState("");
 
+  const [search, setSearch] = useState(""); //search product according to title
+  const [selectedCategory, setSelctedCategory] = useState(""); //to change the data accroding to the selected category
+  const [sortprice, setsortPrice] = useState(""); //for sorting - high to low and low to high
+
+
+  //Pagination states
+  const [currentPage, setCurrentPage] = useState(1)
+  const itemsPerPage = 5
+
+
+
+  //API Link
   const baseUrl = "https://dummyjson.com/products";
 
+  //Create one function for API Fetching
   const getProductData = async () => {
+
     setLoading(true);
     setError("");
     try {
       const response = await axios.get(baseUrl);
       //console.log(response)
       const result = response.data;
-      console.log(result.products);
-      setProducts(result.products);
+      //console.log(result.products);
+      setProducts(result.products); //Get 30 products data
     } catch (error) {
       setError("Failed to get products details, check the url once !!!");
     } finally {
@@ -38,65 +47,117 @@ const Products = () => {
     getProductData();
   }, []);
 
+
   //to get all categories 
   const uniqueCategories = useMemo(()=>{
     return [...new Set(products.map((prod)=> prod.category))]
   },[products])
-  console.log(uniqueCategories)
+  //console.log(uniqueCategories)
 
-  //Filter + Sorting
-  const filterSortProductsData = useMemo(()=>{
-    let prodData = [...products]
 
+  //Filter, search and Sort the products
+  const filterSortProductData = useMemo(()=>{
+    let productData = [...products]
+
+    //search product according to title
+    if(search){
+      productData = productData.filter((item)=>item.title.toLowerCase().includes(search.toLowerCase()))
+    }
+
+    //Filter data according to Category
     if(selectedCategory){
-      prodData.filter((prod)=>prod.category === selectedCategory)
+      productData = productData.filter((item)=>item.category === selectedCategory)
     }
-  },[products])
+
+    //Sort Products according to Price
+    //low to high - ascending (a - b)
+    if(sortprice === "LowToHigh"){
+      productData.sort((a,b)=>a.price - b.price)
+    }
+    //high to low - descending (b - a)
+    if(sortprice === "HighToLow"){
+      productData.sort((a,b)=>b.price - a.price)
+    }
+
+    return productData
+  },[products,search,selectedCategory,sortprice])
+
+
+  //Pagination Logic
+  const paginatedProducts = useMemo(()=>{
+
+    const start = (currentPage - 1)*itemsPerPage
+    const endIndex = start + itemsPerPage
+    //              (2 - 1) * 5 = 1*5 = 5      5   +    5
+    //              (3 - 1) * 5 = 2*5 = 10
+    //              (4 - 1) * 5 = 3*5 = 15
+    return filterSortProductData.slice(start, endIndex)
+    //[0,1,2,3,4] slice(0,5)
+    //[5,6,7,8,9] slice(5,10)
+  },[currentPage,filterSortProductData])
+
+  const totalPages = Math.ceil(filterSortProductData.length / itemsPerPage)
+  console.log(totalPages)
+
+
+
+
+
   
-
-  /* bootstrap version spinner - install bootstrap package, import bootstrap file  */
-  /* 
-    if(loading) {
-    return <div class="spinner-border text-primary" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-    }
-  */
-
   if (loading) return <h3>Loading....</h3>;
   if (error) return <h3 style={{ color: "red" }}>{error}</h3>;
+
+
+
   return (
     <div>
       <h2 className="text-center text-2xl font-semibold mt-3 text-blue-500">
         Product Details
       </h2>
       <div className="mx-5 mb-5 flex gap-2 bg-black p-3 text-white rounded-2xl justify-center">
+
         {/* For search the products */}
         <input
           type="text"
           placeholder="Enter Item Title"
           className="border-2 p-2 rounded-2xl border-white "
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
         />
+
+
 
         {/* Filter products on category */}
         <select 
           className="border-2 p-2 rounded-2xl w-50 text-blue-400" 
           value={selectedCategory}
-          onChange={(e)=>setSelctedCategory(e.target.value)}>
+          onChange={(e)=>setSelctedCategory(e.target.value)}
+        
+        >
+            
           <option value="">All</option>
-          {uniqueCategories.map((c, index) => {
+          {
+            uniqueCategories.map((item, index) => {
             return (
-              <option key={index} value={c}>
-                {c}
-              </option>
+              <option key={index} value={item}>{item}</option>
             );
           })}
         </select>
 
+
+
         {/* Sort data accriding to price */}
-        <select className="border-2 p-2 rounded-2xl w-50">
+        <select 
+          className="border-2 p-2 rounded-2xl w-50 text-red-500"
+          value={sortprice}
+          onChange={(e)=>setsortPrice(e.target.value)}
+        >
           <option value="">Sort</option>
+          <option value="HighToLow">HighToLow</option>
+          <option value="LowToHigh">LowToHigh</option>
         </select>
+
+
       </div>
       <table className="border-2 table-auto mx-5 text-center">
         <thead className="border-2 ">
@@ -113,7 +174,7 @@ const Products = () => {
         </thead>
         <tbody>
           {
-          filterSortProductsData.map((product) => {
+          filterSortProductData.map((product) => {
             return (
               <tr key={product.id}>
                 <td className="border-2">{product.id}</td>
